dd91a33f7a54d711325a44506c17e88d5289a951
harry <harry@HARRY>
2024-10-13 02:18:49 +0500

    Updated Index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+const users = require('./routes/users');
 const PORT = process.env.PORT || 5000;
+
+app.use('/users', users);
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
