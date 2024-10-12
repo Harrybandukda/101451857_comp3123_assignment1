cc25e09d11b68b92eac5adc9adbc0e133ebc0cc3
harry <harry@HARRY>
2024-10-13 02:15:16 +0500

    Added desired output for both user routes

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,15 @@ router.post('/register', async (req, res) => {
     try {
         const user = new User({ username, email, password });
         await user.save();
-        res.status(201).send(user);
+        res.status(201).send({
+            message: "User created sucessfully.",
+            user_id: user._id
+        });
     } catch (err) {
-        res.status(400).send(err);
+        res.status(400).send({
+            message: "Error creating user.",
+            error: err.message
+        });
     }
 });
 
@@ -18,13 +24,20 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await User.findOne({email, password});
+        const user = await User.findOne({ email, password });
         if (!user) {
-            return res.status(401).send("Invalid password or email");
+            return res.status(401).send({
+                message: "Invalid email or password"
+            });
         }
-        res.status(200).send(user); 
+        res.status(200).send({
+            message: "Login successful."
+        });
     } catch (err) {
-        res.status(500).send(err);  
+        res.status(500).send({
+            message: "Error loging in.",
+            error: err.message
+        });
     }
 });
 
