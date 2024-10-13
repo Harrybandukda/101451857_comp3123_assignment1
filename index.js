99704ab6a57083f3bafd71f643fda09cb22c4c05
harry <harry@HARRY>
2024-10-13 20:58:43 +0500

    Added meanful comments to index

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// Mo Harry Bandukda
+// Student ID: 101451857
+// COMP 3123 - Assignment One - Full Stack Development I
+
+// loading env variables
 require('dotenv').config(); 
+
+// importing required moduls
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -7,9 +14,12 @@ const users = require('./routes/user');
 const emp = require('./routes/emp'); 
 
 app.use(express.json());
+
+// Routes setup for user and emp 
 app.use('/users', users);
 app.use('/emp', emp); 
 
+// Connect to MongoDB
 const mongoURI = process.env.MONGODB_URI; 
 if (!mongoURI) {
     console.error('MONGODB_URI is undefined. Please check your .env file.');
@@ -23,4 +33,5 @@ mongoose.connect(mongoURI, {
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
+// Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
